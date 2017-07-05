f350161d8eab676cb1e9ec4e5b5e91ab6aa71f18
Linda "zig" Speight <linda@reachmaine.com>
2017-07-05 09:20:05 -0400

    dont move & rename css file

--- lobsterpot-forked/config.rb
+++ lobsterpot-forked/config.rb
@@ -9,7 +9,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # :expanded or :compact or :compressed
-output_style = :nested 
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
@@ -25,11 +25,10 @@ line_comments = false
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
 # Uncomment below For WordPress. SRC: http://css-tricks.com/compass-compiling-and-wordpress-themes/
-require 'fileutils'
-on_stylesheet_saved do |file|
-  if File.exists?(file) && File.basename(file) == "master.css"
-    puts "Moving #{file}"
-    FileUtils.mv(file, File.dirname(file) + "/../style.css")
- + File.basename(file)
-  end
-end
+#require 'fileutils'
+#  if File.exists?(file) && File.basename(file) == "master.css"
+#    puts "Moving #{file}"
+#    FileUtils.mv(file, File.dirname(file) + "/../style.css")
+ #+ File.basename(file)
+#  end
+#end
